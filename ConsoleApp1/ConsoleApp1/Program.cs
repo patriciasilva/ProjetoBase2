9b90ab139a4e34c6f38e41cbdf24beb6ebd21321
patriciavirgilio1 <patriciavirgilio1@gmail.com>
2018-07-29 23:15:41 -0300

    criacao do projeto no visual studio

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://google.com.br");
+        }
+    }
+}
