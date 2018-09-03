c0cb04e1464707b9d328806df8e3da5467b64cad
patriciavirgilio1 <patriciavirgilio1@gmail.com>
2018-09-02 22:40:43 -0300

    Criação de testes

---
+++ ProjetoBase2Mantis/TestCase/LoginTest.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoBase2Mantis
+{
+    class LoginTest
+    {
+        private ChromeDriver chrome;
+        private LoginPage home;
+
+        [SetUp]
+        public void AntesDosTestes()
+        {
+            chrome = new ChromeDriver();
+            home = new LoginPage(chrome);
+        }
+
+        [Test]
+        public void Cenario1DeveLogar()
+        {
+
+            home.visita();
+            home.login("administrator", "213821"); 
+        }
+
+        [Test]
+        public void Cenario2UsernameIncorreto()
+        {
+            home.visita();
+            home.login("teste", "213821");
+
+        }
+
+        [TearDown]
+        public void Sair()
+        {
+            chrome.Close();
+        }
+    }
+}
